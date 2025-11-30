7cedd71f68e651a7055744df6c7e8e34dd40749b
Jeremie Leo J. Talatala <jeremieleotalatala@gmail.com>
2025-11-30 19:17:05 +0800

    Imported Streamlit and libraries and wrote title code

--- Code Folder/BitPredict-main.py
+++ Code Folder/BitPredict-main.py
@@ -1,1 +1,9 @@
-print("Hello world.")
+import ccxt
+import streamlit as st
+import networkx as nx
+import pandas as pd
+import numpy as np
+import itertools
+
+st.title("BitPredict: Digital Currency Insight Program")
+st.write("Cryptocurrency Prediction Software Prototype")
